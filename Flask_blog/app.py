2539fb05a0898140b149847d11dd202dfd353d33
Papashanskiy <apashanskiy@gmail.com>
2019-02-07 10:12:44 +0300

    2a start blog

---
+++ Flask_blog/app.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'posts.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class Posts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(128))
+    sub_title = db.Column(db.String(128))
+    author = db.Column(db.String(64), index=True)
+    time = db.Column(db.String(64))
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/post')
+def post():
+    return render_template('post.html')
+
+
+@app.route('/new_post')
+def new_post():
+    return render_template('new_post.html')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
