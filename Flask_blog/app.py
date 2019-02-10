cf999867befa7d8213b2b6675b723f2b9f392fd7
Papashanskiy <apashanskiy@gmail.com>
2019-02-10 12:14:56 +0300

    3a final version

--- Flask_blog/app.py
+++ Flask_blog/app.py
@@ -1,29 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, flash, url_for
 from flask_sqlalchemy import SQLAlchemy
-import os
+import os, datetime
 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'posts.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'post.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
 
-class Posts(db.Model):
+class Post(db.Model):                               # DB model
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(128))
     sub_title = db.Column(db.String(128))
     author = db.Column(db.String(64), index=True)
-    time = db.Column(db.String(64))
+    date_post = db.Column(db.DateTime)
+    content = db.Column(db.Text)
+
+    def __repr__(self):
+        return f'<Author: {self.author}, post title: {self.title}>'
 
 
-@app.route('/')
+@app.route('/')                                     # View functions
 def index():
-    return render_template('index.html')
+    posts = Post.query.all()
+    return render_template('index.html', posts=posts)
 
 
 @app.route('/about')
@@ -31,9 +36,10 @@ def about():
     return render_template('about.html')
 
 
-@app.route('/post')
-def post():
-    return render_template('post.html')
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    post = Post.query.filter_by(id=post_id).one()
+    return render_template('post.html', post=post)
 
 
 @app.route('/new_post')
@@ -41,7 +47,26 @@ def new_post():
     return render_template('new_post.html')
 
 
-@app.errorhandler(404)
+@app.route('/addpost', methods=['POST', 'GET'])
+def addpost():
+    if request.method == 'POST':
+        title = request.form['title']
+        subtitle = request.form['subtitle']
+        author = request.form['author']
+        content = request.form['content']
+
+        post = Post(title=title, sub_title=subtitle, author=author, content=content, date_post=datetime.datetime.now())
+
+        db.session.add(post)
+        db.session.commit()
+
+        return redirect(url_for('index'))
+    else:
+        flash('<h1>Something came wrong</h1>')
+        return redirect(url_for('index'))
+
+
+@app.errorhandler(404)                             # Error handlers
 def page_not_found(e):
     return render_template('404.html'), 404
 
